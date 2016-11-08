e277d0653b3de57f3c3905b53ef1030ff03cec1a
Luky Wong <lukywong@gmail.com>
2016-11-08 15:52:25 +0800

    Setup project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Reflection;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public Text countText;
+
+	private Rigidbody2D rb2d;
+	private int count;
+
+	void Start() 
+	{
+		this.rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		SetCountText ();
+	}
+
+	void FixedUpdate() 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() 
+	{
+		countText.text = "Count: " + count.ToString ();
+	}
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class Rotater : MonoBehaviour 
+{
+	void Update () 
+	{
+		transform.Rotate (Vector3.forward * 50 * Time.deltaTime);
+	}
+}
